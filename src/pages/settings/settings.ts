2aa3bccfa33ded796960e65b8f90a6cfa43faa83
ThatRobinGuy <gilmartin.glenn@gmail.com>
2018-05-11 22:37:29 +0100

    Ionic Storage Added

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SettingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-settings',
@@ -15,7 +8,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
+  city : string;
+  state : string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.city = "Orlando";
+    this.state = "FL";
   }
 
   ionViewDidLoad() {
